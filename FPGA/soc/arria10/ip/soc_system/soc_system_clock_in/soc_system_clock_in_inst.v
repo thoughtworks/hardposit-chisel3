94df29018fb6493beb308a0583a7ca4f2a7fe167
Bhimsen Padalkar <bhimsenp@thoughtworks.com>
2019-07-19 09:28:26 +0530

    Added ip files

---
+++ FPGA/soc/arria10/ip/soc_system/soc_system_clock_in/soc_system_clock_in_inst.v
@@ -0,0 +1,5 @@
+	soc_system_clock_in u0 (
+		.in_clk  (_connected_to_in_clk_),  //   input,  width = 1,  in_clk.clk
+		.out_clk (_connected_to_out_clk_)  //  output,  width = 1, out_clk.clk
+	);
+
---
+++ FPGA/soc/arria10/ip/soc_system/soc_system_num_1/soc_system_num_1_inst.v
@@ -0,0 +1,11 @@
+	soc_system_num_1 u0 (
+		.clk        (_connected_to_clk_),        //   input,   width = 1,                 clk.clk
+		.reset_n    (_connected_to_reset_n_),    //   input,   width = 1,               reset.reset_n
+		.address    (_connected_to_address_),    //   input,   width = 2,                  s1.address
+		.write_n    (_connected_to_write_n_),    //   input,   width = 1,                    .write_n
+		.writedata  (_connected_to_writedata_),  //   input,  width = 32,                    .writedata
+		.chipselect (_connected_to_chipselect_), //   input,   width = 1,                    .chipselect
+		.readdata   (_connected_to_readdata_),   //  output,  width = 32,                    .readdata
+		.out_port   (_connected_to_out_port_)    //  output,  width = 32, external_connection.export
+	);
+
