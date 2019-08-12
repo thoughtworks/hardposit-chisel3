9f067e2a029dd65a0d0750740e991c85e9eb4aac
Sai Ganesh <44020019+luckyganesh@users.noreply.github.com>
2019-08-12 10:57:53 +0530

    Creating test class inside of spec and made it private and assertion tests.

--- fpga/src/test/scala/RegimeExtractorSpec.scala
+++ fpga/src/test/scala/RegimeExtractorSpec.scala
@@ -1,38 +1,38 @@
 import chisel3.iotesters.PeekPokeTester
 import org.scalatest.{FlatSpec, Matchers}
 
-class RegimeExtractorTest(c: RegimeExtractor, input: Int, expectedLength: Int, expectedRegime: Int) extends PeekPokeTester(c) {
-  poke(c.io.num,input)
-  expect(c.io.regimeLength,expectedLength)
-  expect(c.io.regime,expectedRegime)
-}
-
 class RegimeExtractorSpec extends FlatSpec with Matchers {
   behavior of "Regime Extractor"
 
-  def test(input: Int, expectedLength: Int, expectedRegime: Int) = {
+  private class RegimeExtractorTest(c: RegimeExtractor, input: Int, expectedLength: Int, expectedRegime: Int) extends PeekPokeTester(c) {
+    poke(c.io.num,input)
+    expect(c.io.regimeLength,expectedLength)
+    expect(c.io.regime,expectedRegime)
+  }
+
+  private def test(input: Int, expectedLength: Int, expectedRegime: Int): Boolean = {
     chisel3.iotesters.Driver(() => new RegimeExtractor(7)){
       c => new RegimeExtractorTest(c, input, expectedLength, expectedRegime)
-    } should be(true)
+    }
   }
 
   it should "extract the positive regime" in {
-    test(0x60, 2,1)
+    assert(test(0x60, 2,1))
   }
 
   it should "extract the negative regime" in {
-    test(0x0F, 3,-3)
+    assert(test(0x0F, 3,-3))
   }
 
   it should "extract the regime as zero as the bit pattern is 10" in {
-    test(0x40,1,0)
+    assert(test(0x40,1,0))
   }
 
   it should "extract the regime as -1 as the bit pattern is -1" in {
-    test(0x30,1,-1)
+    assert(test(0x30,1,-1))
   }
 
   it should "extract the positive regime of max length when all the bits are one" in {
-    test(0x7F,7,6)
+    assert(test(0x7F,7,6))
   }
 }
