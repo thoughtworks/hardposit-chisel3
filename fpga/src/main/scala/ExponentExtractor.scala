7a7fff639d843ab661e36a47aa149a844477a17f
Sai Ganesh <44020019+luckyganesh@users.noreply.github.com>
2019-08-13 12:19:49 +0530

    ExponentExtractor will provide final Exponent which we get after regime extraction.

--- fpga/src/main/scala/ExponentExtractor.scala
+++ fpga/src/main/scala/ExponentExtractor.scala
@@ -1,29 +1,33 @@
 import chisel3._
-import chisel3.util.{MuxCase, log2Ceil}
+import chisel3.util.{Cat, MuxCase, log2Ceil}
 
 class ExponentExtractor(totalBits: Int, es: Int) extends Module {
   require(es >= 0)
-  val io = IO(new Bundle{
-    val regimeLength = Input(UInt((log2Ceil(totalBits) + 1).W))
+  val io = IO(new Bundle {
     val num = Input(UInt(totalBits.W))
     val totalLength = Output(UInt((log2Ceil(totalBits) + 1).W))
-    val exponent = Output(UInt(log2Ceil(es + 2).W))
+    val exponent = Output(SInt((log2Ceil(math.pow(2,es).toInt * totalBits) + 1).W))
   })
 
+  private val regimeExtractor = Module(new RegimeExtractor(totalBits - 1))
+  regimeExtractor.io.num := io.num(totalBits - 2, 0)
+  private val regime = regimeExtractor.io.regime
+
   private val maxBits = totalBits - 1
-  private val regimeLength = Mux(io.regimeLength === maxBits.U,maxBits.U,io.regimeLength + 1.U)
+  private val regimeLength = Mux(regimeExtractor.io.regimeLength === maxBits.U, maxBits.U, regimeExtractor.io.regimeLength + 1.U)
 
-  private val exponentLength = Mux(regimeLength + es.U >= maxBits.U,maxBits.U - regimeLength,es.U)
+  private val exponentLength = Mux(regimeLength + es.U >= maxBits.U, maxBits.U - regimeLength, es.U)
 
-  private val exponentCombinations = Array.range(2,totalBits).map(index => {
-    if(index + es > maxBits){
-       (regimeLength === index.U) -> io.num(maxBits - index,0)
-    }
-    else {
-      (regimeLength === index.U) -> io.num(maxBits - index + 1,maxBits - index + 1 - es)
-    }
+  private val exponentCombinations = Array.range(2, maxBits - 1 - es).map(index => {
+    (regimeLength === index.U) -> Cat(0.U(1.W),io.num(maxBits - (index + 1), maxBits - (index + 1) - es))(es+1,es)
+  })
+
+  private val exponentCombinations1 = Array.range(1, es).reverse.map(index => {
+    (regimeLength === (maxBits - index).U) -> Cat(0.U((es-index).W), io.num(index - 1, 0))
   })
 
   io.totalLength := regimeLength + exponentLength
-  io.exponent :=  Mux(exponentLength === 0.U,0.U,MuxCase(0.U,exponentCombinations))
+  private val exponent = Mux(exponentLength === 0.U, 0.U, MuxCase(0.U, exponentCombinations ++ exponentCombinations1))
+
+  io.exponent := (math.pow(2,es).toInt.S * regime) + exponent.asSInt()
 }
--- fpga/src/test/scala/ExponentExtractorSpec.scala
+++ fpga/src/test/scala/ExponentExtractorSpec.scala
@@ -3,34 +3,41 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class ExponentExtractorSpec extends FlatSpec with Matchers {
 
-  private class ExponentExtractorTest(c: ExponentExtractor, input: Int, regimeLength: Int, expectedLength: Int, exponent: Int) extends PeekPokeTester(c) {
+  private class ExponentExtractorTest(c: ExponentExtractor, input: Int, expectedLength: Int, exponent: Int) extends PeekPokeTester(c) {
     poke(c.io.num,input)
-    poke(c.io.regimeLength,regimeLength)
     expect(c.io.totalLength,expectedLength)
     expect(c.io.exponent,exponent)
   }
 
-  private def test(totalBits:Int,exponentBits:Int , input: Int, regimeLength: Int, expectedLength: Int, exponent: Int): Boolean = {
+  private def test(totalBits:Int,exponentBits:Int , input: Int, expectedLength: Int, exponent: Int): Boolean = {
     chisel3.iotesters.Driver(() => new ExponentExtractor(totalBits,exponentBits)){
-      c => new ExponentExtractorTest(c,input,regimeLength,expectedLength,exponent)
+      c => new ExponentExtractorTest(c,input,expectedLength,exponent)
     }
   }
 
   it should "return the totalLength as regimeLength + exponentLength + 1 when there are exponentBits present in it" in {
-    assert(test(8,1,0x36,1,3,1))
+    assert(test(8,1,0x36,3,-1))
+    assert(test(8,1,0x26,3,-2))
   }
 
   it should "return the totalLength as regimeLength + 1 when there are max regime bits and 0 es bits" in {
-    assert(test(8,0,0x35,1,2,0))
+    assert(test(8,0,0x35,2,-1))
   }
 
   it should "return the exponent as zero when the regime bits occupied all the places" in {
-    assert(test(8,2,0x7F,7,7,0))
-    assert(test(8,2,0x7E,6,7,0))
+    assert(test(8,2,0x7F,7,24))
+    assert(test(8,2,0x7E,7,20))
   }
 
   it should "return the exponent only 1 bit even if es=2 as regime bits occupied the places" in {
-    assert(test(8,2,0x7C,5,7,0))
-    assert(test(8,2,0x7D,5,7,1))
+    assert(test(8,2,0x7C,7,16))
+    assert(test(8,2,0x7D,7,17))
+  }
+
+  it should "[redundant test just to confirm]return the exponent and exponent length as expected" in {
+    assert(test(8,3,0x20,5,-8))
+    assert(test(8,1,0x40,3,0))
+    assert(test(8,3,0x03,7,-39))
+    assert(test(8,3,0x01,7,-48))
   }
 }
