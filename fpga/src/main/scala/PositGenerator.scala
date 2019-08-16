b58b274dfaecade57abe24c2b225202a71de760c
Sai Ganesh <44020019+luckyganesh@users.noreply.github.com>
2019-08-16 10:47:44 +0530

    Created positGenerator to generate posit from the exponent fraction and sign.

---
+++ fpga/src/main/scala/PositGenerator.scala
@@ -0,0 +1,50 @@
+import chisel3._
+import chisel3.util.{Cat, MuxCase, log2Ceil}
+
+class PositGenerator(totalBits: Int, es: Int) extends Module {
+  private val base = math.pow(2, es).toInt
+  private val exponentBits = log2Ceil(base * totalBits) + es + 1
+  val io = IO(new Bundle {
+    val sign = Input(Bool())
+    val exponent = Input(SInt(exponentBits.W))
+    val fraction = Input(UInt(totalBits.W))
+    val posit = Output(UInt(totalBits.W))
+  })
+
+  private val exponent = Mux(io.exponent < 0.S, if (es > 0) base.asSInt() - io.exponent else 0.S - io.exponent, io.exponent).asUInt()
+  private val positRegime = exponent / base.U
+  private val positExponent = (exponent % base.U) (if (es > 0) es - 1 else 0, 0)
+
+  private val positiveExponentCombinations = Array.range(0, totalBits).map(index => {
+    val regimeBits = (math.pow(2, index + 2) - 2).toInt.U
+    val bitsRequiredForRegime = index + 2
+    val bitsRequiredForExponent = es
+    val bitsRequiredForFraction = totalBits
+    val usedBits = bitsRequiredForRegime + bitsRequiredForExponent + bitsRequiredForFraction
+    val numberOfBitsExcludingSignBit = totalBits - 1
+
+    var finalPosit = regimeBits
+    finalPosit = if (es > 0) Cat(finalPosit, positExponent) else finalPosit
+    finalPosit = Cat(finalPosit, io.fraction)
+    printf("*******%d %d %d\n", finalPosit(usedBits - 1, usedBits - numberOfBitsExcludingSignBit), index.U, positRegime)
+    (positRegime === index.U) -> finalPosit(usedBits - 1, usedBits - numberOfBitsExcludingSignBit)
+  })
+
+  private val negativeExponentCombinations = Array.range(1, totalBits - 1).map(index => {
+    val regimeBits = 1.U((index + 1).W)
+    val bitsRequiredForRegime = index + 1
+    val bitsRequiredForExponent = es
+    val bitsRequiredForFraction = totalBits
+    val usedBits = bitsRequiredForRegime + bitsRequiredForExponent + bitsRequiredForFraction
+    val numberOfBitsExcludingSignBit = totalBits - 1
+
+    var finalPosit = regimeBits
+    finalPosit = if (es > 0) Cat(finalPosit, positExponent) else finalPosit
+    finalPosit = Cat(finalPosit, io.fraction)
+    printf("*******%d %d %d %d\n", finalPosit(usedBits - 1, usedBits - numberOfBitsExcludingSignBit), index.U, positRegime,io.exponent)
+    (positRegime === index.U) -> finalPosit(usedBits - 1, usedBits - numberOfBitsExcludingSignBit)
+  })
+
+  val posit = Mux(io.exponent >= 0.S, MuxCase(0.U, positiveExponentCombinations), MuxCase(0.U, negativeExponentCombinations))
+  io.posit := Cat(io.sign, Mux(io.sign, 0.U - posit, posit))
+}
---
+++ fpga/src/test/scala/PositGeneratorSpec.scala
@@ -0,0 +1,44 @@
+import chisel3.iotesters.PeekPokeTester
+import org.scalatest.{FlatSpec, Matchers}
+
+class PositGeneratorSpec extends FlatSpec with Matchers {
+  behavior of "Posit Generator"
+
+  private class PositGeneratorTest(c: PositGenerator, sign: Boolean, exponent: Int, fraction: Int, expectedPosit: Int) extends PeekPokeTester(c) {
+    poke(c.io.sign, sign)
+    poke(c.io.exponent, exponent)
+    poke(c.io.fraction, fraction)
+    step(1)
+    expect(c.io.posit, expectedPosit & 0xFF)
+  }
+
+  private def test(totalBits: Int, es: Int, sign: Boolean, exponent: Int, fraction: Int, expectedPosit: Int): Boolean = {
+    chisel3.iotesters.Driver(() => new PositGenerator(totalBits, es)) {
+      c => new PositGeneratorTest(c, sign, exponent, fraction, expectedPosit)
+    }
+  }
+
+  it should "extract the positive regime" in {
+    assert(test(8,1,sign = false, 3, 0x60, 0x6B))
+  }
+
+  it should "extract the negative regime" in {
+    assert(test(8,1,true,3,0x60,-0x6B))
+  }
+
+  it should "extract the posit for zero exponent and positive sign" in {
+    assert(test(8,2,false,0,0xB3,0x45))
+  }
+
+  it should "extract the posit for zero exponent bits and positive sign" in {
+    assert(test(8,0,false,2,0xB3,0x75))
+    assert(test(8,0,false,-2,0xB3,0x1B))
+  }
+
+  it should "extract the posit for bigger numbers" in {
+    assert(test(8,0,false,7,0xFF,0x7f))
+    assert(test(8,0,false,6,0xFF,0x7f))
+    assert(test(8,0,false,-6,0xFF,0x01))
+    assert(test(8,0,true,-6,0xFF,0xFF))
+  }
+}
