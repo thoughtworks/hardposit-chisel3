0b9ba0b364195c1e945628af748a8d8456ce06fd
Sai Ganesh <44020019+luckyganesh@users.noreply.github.com>
2019-08-27 12:16:58 +0530

    Implemented PositDiv and written tests.

---
+++ fpga/src/main/scala/PositDiv.scala
@@ -0,0 +1,9 @@
+import chisel3._
+
+class PositDiv(totalBits: Int, es:Int) extends PositArithmeticModule(totalBits) {
+  private val positMul = Module(new PositMul(totalBits,es))
+  positMul.io.num1 := io.num1
+  positMul.io.num2 := math.pow(2,totalBits-1).toInt.U - io.num2
+  io.out := positMul.io.out
+  io.isNaN := positMul.io.isNaN
+}
