217ef41d5c3098309d3615dda23262f1618ef320
Sai Ganesh <44020019+luckyganesh@users.noreply.github.com>
2019-09-18 13:24:48 +0530

    Simplified the regime extraction.
    This will eventually reduce so many blocks in the FPGA.

--- fpga/src/main/scala/RegimeExtractor.scala
+++ fpga/src/main/scala/RegimeExtractor.scala
@@ -9,19 +9,11 @@ class RegimeExtractor(totalBits: Int) extends Module {
     val regime = Output(SInt((log2Ceil(totalBits) + 1).W))
   })
 
-  private val regimeMappingForOnes = Array.range(0, totalBits - 1).map(index => {
-    val num = io.num(totalBits - 1, totalBits - (2 + index))
-    val regimePattern = (math.pow(2, index + 2) - 2).toInt.U
-    (num === regimePattern) -> (index + 1).U
+  private val regimeMappongs = Array.range(0, totalBits - 1).reverse.map(index => {
+    (!(io.num(index + 1) === io.num(index))) -> (totalBits - (index + 1)).U
   })
 
-  private val regimeMappingForZeroes = Array.range(0, totalBits - 1).map(index => {
-    val num = io.num(totalBits - 1, totalBits - (2 + index))
-    (num === 1.U) -> (index + 1).U
-  })
+  io.regimeLength := MuxCase(totalBits.U, regimeMappongs)
 
-  private val regimeMappingForAllEqualOnes = (io.num === (math.pow(2,totalBits) - 1).toInt.U) -> totalBits.U
-
-  io.regimeLength := MuxCase(0.U, (regimeMappingForOnes ++ regimeMappingForZeroes):+ regimeMappingForAllEqualOnes)
   io.regime := Mux(io.num(totalBits - 1), io.regimeLength.asSInt() - 1.S, 0.S - io.regimeLength.asSInt())
 }
