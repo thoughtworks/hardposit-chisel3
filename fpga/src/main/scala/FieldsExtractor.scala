a8b44422e42d0167b354d8908b1d3c8f27b76ce6
Arun <kumar.mvarun.arun@gmail.com>
2019-12-12 13:24:19 +0530

    Add. Zero and NaR check to FieldExtractor

--- fpga/src/main/scala/FieldsExtractor.scala
+++ fpga/src/main/scala/FieldsExtractor.scala
@@ -4,15 +4,20 @@ import chisel3.util.log2Ceil
 class FieldsExtractor(totalBits: Int, es: Int) extends Module {
   val io = IO(new Bundle {
     val num = Input(UInt(totalBits.W))
+    val isZero = Output(Bool())
+    val isNaR = Output(Bool())
     val sign = Output(Bool())
     val exponent = Output(SInt((log2Ceil(math.pow(2, es).toInt * totalBits) + es + 2).W))
     val fraction = Output(UInt((totalBits + 1).W))
   })
 
   private val num = Mux(io.num(totalBits - 1), -io.num, io.num)
+  private val NaR = math.pow(2,totalBits-1).toInt.U
 
-  io.sign := io.num(totalBits - 1)
+  io.isZero := (io.num === 0.U)
+  io.isNaR := (io.num === NaR)
 
+  io.sign := io.num(totalBits - 1).asBool()
   private val exponentExtractor = Module(new ExponentExtractor(totalBits, es))
   exponentExtractor.io.num := num
   private val totalLengthAfterExponent = exponentExtractor.io.totalLength
