c6ff2b6e79318404c0e2e9488208776faf4aaee3
Arun <kumar.mvarun.arun@gmail.com>
2020-01-10 14:55:04 +0530

    Add. Posit to posit converter

---
+++ fpga/src/main/scala/PtoPConverter.scala
@@ -0,0 +1,25 @@
+package hardposit
+
+import chisel3._
+
+class PtoPConverter(inWidth: Int, inEs: Int, outWidth: Int, outEs: Int) extends Module {
+  val io = IO(new Bundle {
+    val in = Input(UInt(inWidth.W))
+    val out = Output(UInt(outWidth.W))
+  })
+  private val NaR = math.pow(2, outWidth - 1).toInt.U
+
+
+  val extract = Module(new FieldsExtractor(inWidth, inEs))
+  val generate = Module(new PositGeneratorWrapper(outWidth, outEs))
+
+  extract.io.num := io.in
+  generate.io.sign := extract.io.sign
+  generate.io.exponent := extract.io.exponent
+  generate.io.decimal := true.B
+
+  val shiftedFraction = extract.io.fraction(inWidth - 1, 0) << outWidth >> inWidth
+  generate.io.fraction := shiftedFraction
+
+  io.out := Mux(extract.io.isNaR, NaR, generate.io.posit)
+}
---
+++ fpga/src/test/scala/PtoPConverterSpec.scala
@@ -0,0 +1,46 @@
+import chisel3.iotesters.PeekPokeTester
+import hardposit.PtoPConverter
+import org.scalatest.{FlatSpec, Matchers}
+
+class PtoPConverterSpec extends FlatSpec with Matchers {
+
+  private class PtoPConverterTest(c: PtoPConverter, inPosit: Int, expected: Int) extends PeekPokeTester(c) {
+    poke(c.io.in, inPosit)
+    step(1)
+    expect(c.io.out, expected)
+  }
+
+  def test(inWidth: Int, inEs: Int, outWidth: Int, outEs: Int, inPosit: Int, expected: Int): Boolean = {
+    chisel3.iotesters.Driver(() => new PtoPConverter(inWidth, inEs, outWidth, outEs)) {
+      c => new PtoPConverterTest(c, inPosit, expected)
+    }
+  }
+
+  it should "return wide posit for narrow posit with same es" in {
+    assert(test(8, 2, 16, 2, 0x4F, 0x4F00)) //3.75
+  }
+
+  it should "return wide posit for narrow posit with different es" in {
+    assert(test(8, 2, 16, 3, 0x4F, 0x4780))
+  }
+
+  it should "return narrow posit for wide posit with same es" in {
+    assert(test(16, 2, 8, 2, 0x4F00, 0x4F))
+  }
+
+  it should "return narrow posit for wide posit with different es" in {
+    assert(test(16, 3, 8, 2, 0x4780, 0x4F))
+  }
+
+  it should "return NaR for wide posit with value outside narrower range" in {
+    assert(test(16, 3, 8, 0, 0x6200, 0x80)) //512
+  }
+
+  it should "return NaR for wider NaR" in {
+    assert(test(16, 0, 8, 0, 0x8000, 0x80)) //NaR
+  }
+
+  it should "return NaR for narrower NaR" in {
+    assert(test(8, 0, 16, 0, 0x80, 0x8000))
+  }
+}
