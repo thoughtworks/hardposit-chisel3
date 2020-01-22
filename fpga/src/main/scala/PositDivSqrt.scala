8170842cdfd4a4acc1ff5b22d1a5bbf469be5bc3
Arun <kumar.mvarun.arun@gmail.com>
2020-01-22 20:12:19 +0530

    Add. Posit Division square root

---
+++ fpga/src/main/scala/PositDivSqrt.scala
@@ -0,0 +1,130 @@
+package hardposit
+
+import chisel3._
+import chisel3.util.{Cat, log2Ceil, log2Up}
+
+class PositDivSqrt(totalBits: Int, es: Int) extends Module {
+  val io = IO(new Bundle {
+    val validIn = Input(Bool())
+    val readyIn = Output(Bool())
+
+    val sqrtOp = Input(Bool())
+    val num1 = Input(UInt(totalBits.W))
+    val num2 = Input(UInt(totalBits.W))
+
+    val validOut_div = Output(Bool())
+    val validOut_sqrt = Output(Bool())
+    val isNaR = Output(Bool())
+    val isZero = Output(Bool())
+    val out = Output(UInt(totalBits.W))
+    val exceptions = Output(UInt(5.W))
+  })
+  private val NaR = math.pow(2, totalBits - 1).toInt.U
+
+  private val cycleCount = RegInit(0.U(log2Up(totalBits + 3).W))
+
+  private val sqrtOp_stored = Reg(Bool())
+  private val isNaR_out = Reg(Bool())
+  private val isZero_out = Reg(Bool())
+  private val exceptions_out = RegInit(0.U(5.W))
+
+  private val resultSign = Reg(Bool())
+  private val resultExponent = Reg(SInt((log2Ceil(math.pow(2, es).toInt * totalBits) + es + 2).W))
+  private val resultFraction = RegInit(0.U((totalBits + 2).W))
+
+  private val remLo = RegInit(0.U((totalBits + 2).W))
+  private val remHi = RegInit(0.U((totalBits + 2).W))
+  private val divisor = RegInit(0.U((totalBits + 2).W))
+
+  val num1Fields = Module(new FieldsExtractor(totalBits, es))
+  num1Fields.io.num := io.num1
+  private val num1Sign = num1Fields.io.sign
+  private val num1Exponent = num1Fields.io.exponent
+  private val num1Fraction = num1Fields.io.fraction
+
+  val num2Fields = Module(new FieldsExtractor(totalBits, es))
+  num2Fields.io.num := io.num2
+  private val num2Sign = num2Fields.io.sign
+  private val num2Exponent = num2Fields.io.exponent
+  private val num2Fraction = num2Fields.io.fraction
+
+  private val isNaR = Mux(io.sqrtOp, num1Sign | num1Fields.io.isNaR, num1Fields.io.isNaR)
+  private val isZero = Mux(io.sqrtOp, num1Fields.io.isZero, num1Fields.io.isZero | num2Fields.io.isNaR)
+  private val divideByZero = !io.sqrtOp && num2Fields.io.isZero
+  private val specialCase = isNaR || isZero || divideByZero
+  private val exponentDifference = num1Exponent - num2Exponent
+
+  private val idle = cycleCount === 0.U
+  private val readyIn = cycleCount <= 1.U
+
+  private val starting = readyIn && io.validIn
+  private val started_normally = starting && !specialCase
+
+  private val radicand = Mux(io.sqrtOp && num1Exponent(0).asBool(), num1Fraction << 1, num1Fraction)
+
+  when(!idle | io.validIn) {
+    cycleCount := Mux(starting && specialCase, 1.U, 0.U |
+      Mux(started_normally, (totalBits + 1).U, 0.U)) |
+      Mux(!idle, cycleCount - 1.U, 0.U)
+  }
+
+  io.readyIn := readyIn
+
+  when(starting) {
+    sqrtOp_stored := io.sqrtOp
+    isNaR_out := isNaR
+    isZero_out := isZero
+    exceptions_out := Mux(divideByZero, 8.U, 0.U)
+  }
+
+  when(started_normally) {
+    resultSign := Mux(io.sqrtOp, false.B, num1Sign ^ num2Sign)
+    resultExponent := Mux(io.sqrtOp, num1Exponent >> 1, exponentDifference)
+  }
+
+  when(started_normally && !io.sqrtOp) {
+    divisor := num2Fraction
+  }
+
+  remLo := Mux(readyIn && io.sqrtOp, radicand << 2, 0.U) |
+    Mux(!readyIn && sqrtOp_stored, remLo << 2, 0.U)
+
+  private val rem = Mux(readyIn && io.sqrtOp, radicand(totalBits + 1, totalBits), 0.U) |
+    Mux(readyIn && !io.sqrtOp, radicand.asUInt(), 0.U) |
+    Mux(!readyIn && sqrtOp_stored, remHi << 2.U | remLo >> totalBits.U, 0.U) |
+    Mux(!readyIn && !sqrtOp_stored, remHi << 1.U, 0.U)
+
+
+  private val testDiv = Mux(readyIn && io.sqrtOp, 1.U, 0.U) |
+    Mux(readyIn && !io.sqrtOp, num2Fraction, 0.U) |
+    Mux(!readyIn && sqrtOp_stored, Cat(resultFraction << 1.U, 1.U), 0.U) |
+    Mux(!readyIn && !sqrtOp_stored, divisor, 0.U)
+
+  private val testRem = rem.zext - testDiv.zext
+  private val nextBit = testRem >= 0.S
+
+  when(started_normally || cycleCount > 2.U) {
+    remHi := Mux(nextBit, testRem.asUInt(), rem)
+  }
+
+  when(started_normally || !readyIn) {
+    resultFraction := Cat(resultFraction, nextBit.asUInt())
+  }
+
+  private val validOut = cycleCount === 1.U
+
+  private val positGenerator = Module(new PositGeneratorWrapper(totalBits, es))
+  positGenerator.io.decimal := resultFraction(totalBits + 1, totalBits)
+  positGenerator.io.fraction := resultFraction(totalBits - 1, 0)
+  positGenerator.io.exponent := resultExponent
+  positGenerator.io.sign := resultSign
+
+  private val out = Mux(isZero_out, 0.U, Mux(isNaR_out || divideByZero, NaR, positGenerator.io.posit))
+
+  io.validOut_div := validOut && !sqrtOp_stored
+  io.validOut_sqrt := validOut && sqrtOp_stored
+  io.isNaR := isNaR_out
+  io.isZero := isZero_out
+  io.exceptions := exceptions_out
+  io.out := out
+}
---
+++ fpga/src/test/scala/PositDivSqrtSpec.scala
@@ -0,0 +1,84 @@
+import chisel3.iotesters.{ChiselFlatSpec, PeekPokeTester}
+import hardposit.PositDivSqrt
+
+class PositDivSqrtSpec extends ChiselFlatSpec {
+
+  private class PositDivSqrtTest(c: PositDivSqrt, num1: Int, num2: Int, sqrtOp: Boolean, expect: Int) extends PeekPokeTester(c) {
+    poke(c.io.num1, num1)
+    poke(c.io.num2, num2)
+    poke(c.io.sqrtOp, sqrtOp)
+    poke(c.io.validIn, true)
+    step(1)
+    poke(c.io.validIn, false)
+    while (if(sqrtOp) peek(c.io.validOut_sqrt) == BigInt(0) else peek(c.io.validOut_div) == BigInt(0)) {
+      step(1)
+    }
+    expect(c.io.out, expect)
+  }
+
+  private def test(totalBits: Int, es: Int, num1: Int, num2: Int, sqrtOp: Boolean, expect: Int): Boolean = {
+//    chisel3.iotesters.Driver.execute(Array("--generate-vcd-output", "on", "--target-dir", "test_run_dir/div_sqrt", "--top-name", "dsvcd"), () => new PositDivSqrt(totalBits, es)) {
+//      c => new PositDivSqrtTest(c, num1, num2, sqrtOp, expect)
+//    }
+
+      chisel3.iotesters.Driver.execute(Array.empty,() => new PositDivSqrt(totalBits, es)) {
+        c => new PositDivSqrtTest(c, num1, num2, sqrtOp, expect)
+      }
+  }
+
+  it should "return square root when number is given" in {
+    assert(test(8, 0, 0x7C, 0, sqrtOp = true, 0x70)) //16, 4
+  }
+
+  it should "return square root when number is given 2" in {
+    assert(test(8, 0, 0x75, 0, sqrtOp = true, 0x64)) //6.5, 2.5
+  }
+
+  it should "return square root when number is given 3" in {
+    assert(test(8, 0, 0x62, 0, sqrtOp = true, 0x50)) //2.25, 1.5
+  }
+
+  it should "return square root when number is given 4" in {
+    assert(test(8, 2, 0x30, 0, sqrtOp = true, 0x38)) //0.25, 0.5
+  }
+
+  it should "return NaR when input is negative" in {
+    assert(test(8, 0, 0x90, 0, sqrtOp = true, 0x80)) //-4, NaR
+  }
+
+  it should "return NaR when input is NaR" in {
+    assert(test(8, 0, 0x80, 0, sqrtOp = true, 0x80)) //NaR, NaR
+  }
+
+  it should "return positive value when signs are equal" in {
+    assert(test(16, 1, 0x5A00, 0x1E00, sqrtOp = false, 0x6EDB))
+    assert(test(16, 1, 0xA600, 0xE200, sqrtOp = false, 0x6EDB))
+  }
+
+  it should "return negative value when signs are not equal" in {
+    assert(test(16, 1, 0x5A00, 0xE200, sqrtOp = false, 0x9125))
+  }
+
+  it should "return zero when the dividend is zero and divisor is not equal zero" in {
+    assert(test(8, 2, 0, 0x32, sqrtOp = false, 0))
+  }
+
+  it should "return infinity when the divisor is zero and dividend is not equal zero" in {
+    assert(test(8, 2, 0x34, 0, sqrtOp = false, 0x80))
+  }
+
+  it should "return infinity when the dividend is infinite and divisor is not equal infinity" in {
+    assert(test(8, 1, 0x80, 0x42, sqrtOp = false, 0x80))
+    assert(test(8, 1, 0x80, 0, sqrtOp = false, 0x80))
+  }
+
+  it should "return zero when the divisor is infinity and dividend is not infinity" in {
+    assert(test(8, 4, 0x76, 0x80, sqrtOp = false, 0))
+    assert(test(8, 4, 0, 0x80, sqrtOp = false, 0))
+  }
+
+  it should "return isNaN as true when both dividend and divisor are zero or infinite" in {
+    assert(test(8, 3, 0, 0, sqrtOp = false, 0))
+    assert(test(8, 3, 0x80, 0x80, sqrtOp = false, 0))
+  }
+}
