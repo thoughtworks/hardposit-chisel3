3d206bd980b74b30e01ec9e1b4c1e7ee303dc9dc
arunkmv <arunkumarmv1997@gmail.com>
2020-02-11 14:44:26 +0530

    Add. Unpacked posit bundle

--- src/main/scala/PositArithmeticModule.scala
+++ src/main/scala/PositArithmeticModule.scala
@@ -1,6 +1,7 @@
 package hardposit
 
 import chisel3._
+import chisel3.util.log2Ceil
 
 abstract class PositArithmeticModule(totalBits: Int) extends Module {
   val io = IO(new PositArithmeticBundle(totalBits))
@@ -11,4 +12,15 @@ class PositArithmeticBundle(val totalBits: Int) extends Bundle {
   val num2 = Input(UInt(totalBits.W))
   val out = Output(UInt(totalBits.W))
   val isNaN = Output(Bool())
+}
+
+class unpackedPosit(val totalBits: Int, val es: Int) extends Bundle {
+  val sign = Bool()
+  val exponent = SInt((log2Ceil(math.pow(2, es).toInt * totalBits) + es + 2).W)
+  val fraction = UInt((totalBits + 1).W)
+  val isZero = Bool()
+  val isNaR = Bool()
+
+  override def cloneType =
+    new unpackedPosit(totalBits, es).asInstanceOf[this.type]
 }
