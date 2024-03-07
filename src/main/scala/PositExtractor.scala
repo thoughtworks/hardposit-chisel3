a78a02fac9ebaee78c609ea9e33a5f7534d84eed
Swethaa <dmagicisinu@gmail.com>
2024-03-07 16:37:57 +0530

    Revert "Add. Formal constraints for Posit Extractor"
    This reverts commit d58bef97b301d07d20d29c600e780c72757ad021.

--- src/main/scala/PositExtractor.scala
+++ src/main/scala/PositExtractor.scala
@@ -9,50 +9,30 @@ class PositExtractor(val nbits: Int, val es: Int) extends Module with HasHardPos
     val out = Output(new unpackedPosit(nbits, es))
   })
 
-  val sign   = io.in(nbits - 1)
-  val absIn  = Mux(sign, (~io.in).asUInt + 1.U, io.in).asUInt
-  val negExp = ~absIn(nbits - 2)
+  val sign   = io.in(nbits - 1) // Extract sign bit
+  val absIn  = Mux(sign, ~io.in + 1.U, io.in).asUInt // If negative, adjust 2's complement
+  val negExp = ~absIn(nbits - 2) // If the first bit of regime is zero, the the value is between (1, -1)
 
-  val regExpFrac  = absIn(nbits - 2, 0)
-  val zerosRegime = Mux(negExp.asBool, regExpFrac, ~regExpFrac)
+  val regExpFrac  = absIn(nbits - 2, 0)  // Extract (regime + es + fractional) parts
+  val zerosRegime = Mux(negExp, regExpFrac, ~regExpFrac)
   val regimeCount =
     Cat(0.U(1.W),
-      Mux(isZero(zerosRegime.asUInt), (nbits - 1).U, countLeadingZeros(zerosRegime)))
+      Mux(isZero(zerosRegime), (nbits - 1).U, countLeadingZeros(zerosRegime)))
+  val regime =
+    Mux(negExp, ~regimeCount + 1.U, regimeCount - 1.U) // k = m-1 if
 
-  assert((regimeCount <= (nbits.asUInt - 1.U))  & (regimeCount >= 1.U))
+  // Formal: k within range (nbits-1) to 1
 
-  val regime = Mux(negExp.asBool, (~regimeCount).asUInt + 1.U, regimeCount - 1.U)
-
-  assert(regime >= 0.U)
-  val regime_range = (2.U << (nbits.U - 2.U)).asUInt
-  assert(regime < regime_range)
-
-  val expFrac = absIn << (regimeCount + 2.U)
+  val expFrac = absIn << (regimeCount + 2.U) // Extract (es + fractional) parts
   val extractedExp =
     if (es > 0) expFrac(nbits - 1, nbits - es)
     else 0.U
-
-  if(es > 0) {
-    assert((extractedExp < (2.U << es-1).asUInt) & (extractedExp >= 0.U))
-  }
-
-  val frac = expFrac(nbits - es - 1, nbits - es - maxFractionBits)
-
-  assert((frac <= (2.U << maxFractionBits - 1).asUInt) & (frac >= 0.U))
-  when((es.U + regimeCount) === nbits.U - 1.U) {
-    assert(frac === 0.U)
-  }
+  val frac = expFrac(nbits - es - 1, nbits - es - maxFractionBits) // Extract fractional part
 
   io.out.isNaR    := isNaR(io.in)
   io.out.isZero   := isZero(io.in)
-
-  when(io.out.isNaR) {
-    assert(!io.out.isZero)
-  }
-  when(io.out.isZero) {
-    assert(!io.out.isNaR)
-  }
-
+  // Formal: If regime + es = nbits, then fractional has to be 1
+  // Formal: If regime = nbits, then exponent has to be 1 ?
   io.out.sign     := sign
   io.out.exponent := {
     if (es > 0) Cat(regime, extractedExp)
