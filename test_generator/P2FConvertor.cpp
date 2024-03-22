b29d0bdce341bdb3fdf4c541abb99531828b4709
Swethaa <dmagicisinu@gmail.com>
2024-03-22 11:01:42 +0530

    Mani/Swethaa: Add support for 2d arrays in posit to float convertor

--- test_generator/P2FConvertor.cpp
+++ test_generator/P2FConvertor.cpp
@@ -8,10 +8,15 @@
 
 using json_t = nlohmann::json;
 
-json_t parse_data(int argc, char** argv) {
-    if (argc != 3) {
-        std::cout << "Usage: P2FConvertor <json> <field>\n"
+
+void print_helper() {
+    std::cout << "Usage: P2FConvertor <json> <field> <dimension>\n"
                  "Converts a JSON file with posits to floating point numbers\n";
+}
+
+json_t parse_data(int argc, char** argv) {
+    if (argc != 4) {
+        print_helper();
         exit(2);
     }
     std::ifstream file(argv[1]);
@@ -58,11 +63,41 @@ void convert_path_1d(json_t json, std::string field) {
     }
 }
 
-// void convert_path_2d(json_t json, std::string field) {
-//     // TODO: Implement this
-// }
+void convert_path_2d(json_t json, std::string field) {
+    try {
+        auto data = json["memories"][field].get<n_dim_vec_t<unsigned long, 2>>();
+        std::vector<std::vector<double>> data_float(data.size(), std::vector<double>(data[0].size(), 0));
+        for( int i = 0; i < data.size() ; i++) {
+            std::transform(
+                data[i].cbegin(),
+                data[i].cend(),
+                data_float[i].begin(),
+                [](unsigned long v) -> double {
+                    sw::unum::posit <32, 2> p;
+                    p.set_raw_bits(v);
+                    return (double) p;
+            });
+        }
+        json_t transformed;
+        transformed = json;
+        transformed["memories"][field] = data_float;
+        std::cout << transformed.dump(2);
+    }  catch(nlohmann::json::type_error err) {
+        std::cerr << "[Error] Expected `" << field << ".data' field with type float[]" << std::endl;
+        exit(2);
+    }
+}
 
 int main(int argc, char *argv[]) {
     auto j = parse_data(argc, argv);
-    convert_path_1d(j, argv[2]);
+    if(strcmp(argv[3], "1") == 0) {
+        convert_path_1d(j, argv[2]);
+    }
+    else if(strcmp(argv[3], "2") == 0) {
+        convert_path_2d(j, argv[2]);
+    }
+    else {
+        print_helper();
+        exit(2);
+    }
 }
