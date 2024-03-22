6255e376ffb642b388bbc6b41d7f246fb1b3946f
Swethaa <dmagicisinu@gmail.com>
2024-03-22 10:54:07 +0530

    Mani/Swethaa: Refactor string comparison and extracted printer function

--- test_generator/F2PConvertor.cpp
+++ test_generator/F2PConvertor.cpp
@@ -8,10 +8,15 @@
 
 using json_t = nlohmann::json;
 
-json_t parse_data(int argc, char** argv) {
-    if (argc != 4) {
-        std::cout << "Usage: PositConvertor <json> <json-path> <dimension> \n"
+void print_helper() {
+    std::cout << "Usage: F2P <json> <field> <dimension> \n"
                  "Converts a JSON file with floating point numbers to posits\n";
+}
+
+json_t parse_data(int argc, char **argv) {
+    if (argc != 4)
+    {
+        print_helper();
         exit(2);
     }
     std::ifstream file(argv[1]);
@@ -59,7 +64,7 @@ void convert_path_1d(json_t json, std::string field) {
 
  void convert_path_2d(json_t json, std::string field) {
      try {
-             auto data = json[field]["data"].get<n_dim_vec_t<float, 2>>();
+             auto data = json[field]["data"].get<n_dim_vec_t<double, 2>>();
              std::vector<std::vector<unsigned long>> data_posit(data.size(), std::vector<unsigned long>(data[0].size(), 0));
              for( int i = 0; i < data.size() ; i++) {
                  std::transform(
@@ -83,16 +88,14 @@ void convert_path_1d(json_t json, std::string field) {
 
 int main(int argc, char *argv[]) {
     auto j = parse_data(argc, argv);
-    switch(argv[3][0]) {
-    case '1':
-    convert_path_1d(j, argv[2]);
-    break;
-    case '2':
-    convert_path_2d(j, argv[2]);
-    break;
-    default:
-    std::cout << "Usage: PositConvertor <json> <json-path> <dimension> \n"
-                    "Dimension of the array should be either 1 or 2 \n";
-            exit(2);
+    if(strcmp(argv[3], "1") == 0) {
+        convert_path_1d(j, argv[2]);
+    }
+    else if(strcmp(argv[3], "2") == 0) {
+        convert_path_2d(j, argv[2]);
+    }
+    else {
+        print_helper();
+        exit(2);
     }
 }
