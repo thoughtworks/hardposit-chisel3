e23dab9b034b42e1cdf77ae690bb1120f28842ff
Swethaa <dmagicisinu@gmail.com>
2024-04-01 11:47:18 +0530

    [Mani/Swethaa] Add. support mutiple field arguments to P2F and F2P Convertors

--- test_generator/F2PConvertor.cpp
+++ test_generator/F2PConvertor.cpp
@@ -1,12 +1,23 @@
 #include "json.hpp"
 #include "universal/include/universal/posit/posit.hpp"
 #include <algorithm>
+#include <cstdlib>
 #include <fstream>
+#include <getopt.h>
 #include <iostream>
 #include <map>
 #include <string>
+#include <unistd.h>
+#include <vector>
 
 using json_t = nlohmann::json;
+
+struct Input {
+    json_t json;
+    int posit_width;
+    std::vector<std::string> one_dim_fields;
+    std::vector<std::string> two_dim_fields;
+};
 
 template <class T> struct tag { using type = T; };
 template <class Tag> using type = typename Tag::type;
@@ -14,18 +25,6 @@ template <class T, size_t n>
 struct n_dim_vec : tag<std::vector<type<n_dim_vec<T, n - 1>>>> {};
 template <class T> struct n_dim_vec<T, 0> : tag<T> {};
 template <class T, size_t n> using n_dim_vec_t = type<n_dim_vec<T, n>>;
-
-json_t parse_data(std::string file_path) {
-    std::ifstream file(file_path);
-    json_t j;
-    try {
-        file >> j;
-    } catch (nlohmann::detail::parse_error err) {
-        std::cerr << err.what() << std::endl;
-        exit(2);
-    }
-    return j;
-}
 
 template <size_t nbits, size_t es>
 void convert_float_to_posit(std::vector<double> &input,
@@ -38,10 +37,9 @@ void convert_float_to_posit(std::vector<double> &input,
 }
 
 template <size_t nbits, size_t es>
-void convert_path(json_t json, std::string field, int dim) {
+void convert_path(json_t json, std::string field, int dim,
+                  json_t &transformed) {
     try {
-        json_t transformed;
-        transformed = json;
         switch (dim) {
         case 1: {
             auto data = json[field]["data"].get<n_dim_vec_t<double, 1>>();
@@ -62,7 +60,6 @@ void convert_path(json_t json, std::string field, int dim) {
             std::cerr << "[Error] The value of the dimension should be 1 or 2"
                       << std::endl;
         }
-        std::cout << transformed.dump(2);
     } catch (nlohmann::json::type_error err) {
         std::cerr << "[Error] Expected `" << field
                   << ".data' field with type float[]" << std::endl;
@@ -70,39 +67,108 @@ void convert_path(json_t json, std::string field, int dim) {
     }
 }
 
-void convert_to_posit(json_t json, std::string field, int posit_width,
-                      int dim) {
+void convert_to_posit(json_t json, std::string field, int posit_width, int dim,
+                      json_t &transformed) {
     switch (posit_width) {
+    case 8:
+        convert_path<8, 1>(json, field, dim, transformed);
     case 16:
-        convert_path<16, 1>(json, field, dim);
+        convert_path<16, 1>(json, field, dim, transformed);
         break;
     case 32:
-        convert_path<32, 2>(json, field, dim);
+        convert_path<32, 2>(json, field, dim, transformed);
         break;
     case 64:
-        convert_path<64, 3>(json, field, dim);
+        convert_path<64, 3>(json, field, dim, transformed);
         break;
     case 128:
-        convert_path<128, 4>(json, field, dim);
+        convert_path<128, 4>(json, field, dim, transformed);
         break;
 
     default:
         std::cerr << "[Error] The supported posit standards are 16, 32 and 64"
                   << std::endl;
+        exit(2);
+    }
+}
+
+void split_strings(std::string input, char delimiter,
+                   std::vector<std::string> &fields) {
+    std::stringstream stream(input);
+    std::string field;
+
+    while (!stream.eof()) {
+        std::getline(stream, field, delimiter);
+        fields.push_back(field);
+    }
+}
+
+void parse_data(int argc, char **argv, Input &input) {
+    std::ifstream file(argv[1]);
+    try {
+        file >> input.json;
+    } catch (nlohmann::detail::parse_error err) {
+        std::cerr << err.what() << std::endl;
+        exit(2);
+    }
+
+    input.posit_width = std::stoi(argv[2]);
+
+    int c;
+    int index;
+    char *cvalue = NULL;
+    while (1) {
+        static struct option long_options[] = {
+            {"1d", required_argument, 0, 'o'},
+            {"2d", required_argument, 0, 't'},
+            {"help", no_argument, 0, 'h'},
+            {0, 0, 0, 0}};
+        int option_index = 0;
+        c = getopt_long(argc, argv, "o:t:h", long_options, &option_index);
+
+        if (c == -1)
+            break;
+
+        switch (c) {
+        case 'o':
+            split_strings(optarg, ',', input.one_dim_fields);
+            break;
+        case 't':
+            split_strings(optarg, ',', input.two_dim_fields);
+            break;
+        case '?':
+            std::cerr << "Invalid option" << std::endl;
+            exit(2);
+        default:
+            abort();
+        }
+    }
+    if ((optind != argc - 2)) {
+        std::cerr << "Invalid ARGV-elements" << std::endl;
+        exit(2);
     }
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 5) {
+    if (!(argc >= 5)) {
         std::cerr << "Usage: " << argv[0]
-                  << "<json> <field> <dimension> <posit-width>\n"
+                  << "<json> <posit-width> --1d <one-dim field_1>,<one-dim "
+                     "field_2> --2d <two-dim field_1>,<two-dim field_2>\n"
                      "Converts a JSON file with floating points to posits \n";
         return 2;
     }
-    auto json_file = parse_data(argv[1]);
-    std::string field = argv[2];
-    int dim = std::atoi(argv[3]);
-    int width = std::atoi(argv[4]);
+    Input input;
+    parse_data(argc, argv, input);
+    json_t transformed = input.json;
 
-    convert_to_posit(json_file, field, width, dim);
+    for (int i = 0; i < input.one_dim_fields.size(); i++) {
+        convert_to_posit(input.json, input.one_dim_fields[i], input.posit_width,
+                         1, transformed);
+    }
+
+    for (int i = 0; i < input.two_dim_fields.size(); i++) {
+        convert_to_posit(input.json, input.two_dim_fields[i], input.posit_width,
+                         2, transformed);
+    }
+    std::cout << transformed.dump(2);
 }
